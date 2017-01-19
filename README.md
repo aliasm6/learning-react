be4b97c8f5215d2c21d00b729102ce27e5d7229e
Alias M6 <aliasmontoya719@gmail.com>
2017-01-18 22:20:14 -0700

    Updated README.md and hello-react.js * added keyterms and examples to README.md * Reconfigured code in hello-react.js to utilize props and ES6 syntax

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ https://share.viget.com/sxsw/learning-react/lessons/0-intro/index.html
 1. Element - React's most primitive data type. "Chunks of data that describe how to build a UI. Think of them as low level instructions; the assembly of React."
 Example:
 ```javascript
-const example = React.createElement("type",[props],[...children]);
+const elementExample = React.createElement("type",[props],[...children]);
 ```
 
 1. React - The standard library
@@ -19,3 +19,14 @@ const example = React.createElement("type",[props],[...children]);
 1. ReactDOM - The rendering engine
 
 1. Components - Chunks of React elements that represent a common behavior
+Example:
+```javascript
+class ComponentExample extends React.Component {
+  render() {
+    return React.createElement("h1", {}, this.props.exampleContent)
+  }
+}
+```
+1. props - Props create a way to send instructions to components
+1. One-way data flow - Information in React flows downward so when a React component receives props (instructions), it knows to re-render
+1. 
--- hello-react.js
+++ hello-react.js
@@ -1,16 +1,36 @@
-const hello = React.createElement('p', null, 'Hello, World!');
-
 const notes = [
   { id: 1, content: 'Learn React' },
   { id: 2, content: 'Get Lunch' },
   { id: 3, content: 'Learn React Native' }
 ]
 
-const notesListItems = notes.map(message => React.createElement('li', { key: message.id }, message.content))
+class App extends React.Component {
+  render() {
+    let notes = this.props.notes
 
-const notesList = React.createElement('section', {},
-  React.createElement('h1', {}, 'You have ', notes.length, ' reminders'),
-  React.createElement('ul', {}, notesListItems)
-)
+    return React.createElement('section', {},
+      React.createElement('h1', {}, 'You have ', notes.length, ' notes'),
+      React.createElement(NotesList, {notes: notes})
+    )
+  }
+}
 
-ReactDOM.render(notesList, document.getElementById('entry-point'))
+class Note extends React.Component {
+  render() {
+    return React.createElement('li', {}, this.props.content)
+  }
+}
+
+class NotesList extends React.Component {
+  renderNote(note) {
+    return React.createElement(Note, {
+      key: note.id,
+      content: note.content
+    })
+  }
+  render() {
+    return React.createElement('ul', {}, this.props.notes.map(this.renderNote))
+  }
+}
+
+ReactDOM.render(React.createElement(App, { notes: notes }), document.getElementById('entry-point'))
